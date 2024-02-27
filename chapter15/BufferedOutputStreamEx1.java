9f27a9a0bfa308c9049a8b08230bdc90ebe1cb06
kimgoat <kimgoatlove@gmail.com>
2024-02-27 16:51:46 +0900

    BufferedOutputStream를 사용하여 데이터 출력하기

---
+++ chapter15/BufferedOutputStreamEx1.java
@@ -0,0 +1,22 @@
+package chapter15;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BufferedOutputStreamEx1 {
+    public static void main(String[] args) {
+        try {
+            FileOutputStream fos = new FileOutputStream("chapter15/test.txt");
+
+            BufferedOutputStream bos = new BufferedOutputStream(fos, 5);
+
+            for (int i = '1'; i <= '9'; i++) {
+                bos.write(i);
+            }
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
