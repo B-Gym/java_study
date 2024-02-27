7e1d97c90367159bbacb28ae7a64c650308f0990
kimgoat <kimgoatlove@gmail.com>
2024-02-27 20:00:17 +0900

    System.out/ System.err의 출력대상은 모두 콘솔로 같음

---
+++ chapter15/StandardIOEx2.java
@@ -0,0 +1,12 @@
+package chapter15;
+
+public class StandardIOEx2 {
+    public static void main(String[] args) {
+        System.err.println("Hello");
+        System.out.println("Hello");
+    }
+}
+
+// 실행 결과
+// Hello
+// Hello
