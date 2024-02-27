0af2564004d2edf2330a72ed329cb030b4d833fe
kimgoat <kimgoatlove@gmail.com>
2024-02-27 19:46:23 +0900

    InputStreamReader를 사용하여 운영체제의 인코딩 확인하기

---
+++ chapter15/InputStreamReaderEx.java
@@ -0,0 +1,28 @@
+package chapter15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputStreamReaderEx {
+    public static void main(String[] args) {
+        String line = "";
+
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+
+            System.out.println("사용 중인 OS의 인코딩: " + isr.getEncoding());
+
+            do {
+                System.out.print("문장을 입력하세요. 마치려면 q를 입력하세요. > ");
+                line = br.readLine();
+                System.out.println("입력하신 문장 : " + line);
+            } while (!line.equalsIgnoreCase("q"));
+
+            System.out.println("프로그램을 종료합니다. ");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
