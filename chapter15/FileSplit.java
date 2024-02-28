37cd5289936566db29c3b5c5bbcb7b07fb764b4a
kimgoat <kimgoatlove@gmail.com>
2024-02-28 15:18:28 +0900

    지정한 파일을 지정한 크기로 잘라서 여러 개의 파일로 만들기

---
+++ chapter15/FileSplit.java
@@ -0,0 +1,46 @@
+package chapter15;
+
+import java.io.*;
+
+public class FileSplit {
+    public static void main(String[] args) {
+
+        if (args.length < 2) {
+            System.out.println("USAGE: java FileSplit filename SIZE_KB");
+            System.exit(0);
+        }
+
+        final int VOLUME = Integer.parseInt(args[1]) * 1000;
+
+        try {
+            String fileName = args[0];
+            FileInputStream fis = new FileInputStream(fileName);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            FileOutputStream fos = null;
+            BufferedOutputStream bos = null;
+
+            int data = 0;
+            int i = 0;
+            int number = 0;
+
+            while ((data = bis.read()) != -1) {
+                if (i % VOLUME == 0) {
+                    if (i != 0) {
+                        bos.close();
+                    }
+                    fos = new FileOutputStream(fileName + "_." + ++number);
+                    bos = new BufferedOutputStream(fos);
+                }
+                bos.write(data);
+                i++;
+            }
+
+            bis.close();
+            bos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
